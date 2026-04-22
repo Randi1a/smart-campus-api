633c4a84b90682c6e75040ed603505f3754fe909
Randila Udawatta <mercilessorange@gmail.com>
2026-04-22 19:14:43 +0530

    Add request/response logging filter
    Introduce LoggingFilter as a JAX-RS @Provider implementing ContainerRequestFilter and ContainerResponseFilter. Uses java.util.logging to log incoming request method and URI and outgoing response status. Added at src/main/java/com/smartcampus/filter/LoggingFilter.java.

---
+++ src/main/java/com/smartcampus/filter/LoggingFilter.java
@@ -0,0 +1,23 @@
+package com.smartcampus.filter;
+
+import jakarta.ws.rs.container.*;
+import jakarta.ws.rs.ext.Provider;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+
+    private static final Logger log = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public void filter(ContainerRequestContext req) throws IOException {
+        log.info("REQUEST  → " + req.getMethod() + " " + req.getUriInfo().getRequestUri());
+    }
+
+    @Override
+    public void filter(ContainerRequestContext req, ContainerResponseContext res) throws IOException {
+        log.info("RESPONSE ← Status: " + res.getStatus());
+    }
+}
